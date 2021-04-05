626664c825b38f4d5840d8559d77ca5277a317b0
merveberik <merve-berik@yandex.com>
2021-04-06 02:19:50 +0300

    calculation is in function

--- FunctionCalculation/BaseFunctionCalculator.cs
+++ FunctionCalculation/BaseFunctionCalculator.cs
@@ -2,24 +2,29 @@
 
 namespace FunctionCalculation
 {
-    class Program
+    class BaseFunctionCalculator
     {
+
         static void Main(string[] args)
+        {
+
+            Calculate(3);
+
+        }
+
+        public static void Calculate(int n)
         {
             byte i;
             double functionSum;
             functionSum = 0;
-            int n;
-            n = 3;
-
             for (i = 0; i <= n; i++)
             {
                 double radiansValue = (i * (Math.PI)) / 180;
-                double sinValue = Math.Sin(radiansValue) *i;
+                double sinValue = Math.Sin(radiansValue) * i;
                 functionSum = functionSum + sinValue;
             }
-                
             Console.WriteLine("Summation of function value is " + functionSum);
+
 
         }
     }
