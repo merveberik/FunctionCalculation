83aedcb40030921a08f6a8026cd6fad664cd7605
merveberik <merve-berik@yandex.com>
2021-04-06 01:30:13 +0300

    Add project files.

---
+++ FunctionCalculation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FunctionCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte i;
+            double functionSum;
+            functionSum = 0;
+            int n;
+            n = 3;
+
+            for (i = 0; i <= n; i++)
+            {
+                double radiansValue = (i * (Math.PI)) / 180;
+                double sinValue = Math.Sin(radiansValue) *i;
+                functionSum = functionSum + sinValue;
+            }
+                
+            Console.WriteLine("Summation of function value is " + functionSum);
+
+        }
+    }
+}
